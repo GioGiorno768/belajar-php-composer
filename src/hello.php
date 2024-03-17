4016509082bd22f3e02919ea1746fbb4ebc1ad66
Kevin R Krisna <kevinragil768@gmail.com>
2024-03-17 16:32:49 +0700

    pembaruan 1.0

--- src/hello.php
+++ src/hello.php
@@ -5,7 +5,7 @@ class hello {
     public function __construct(private string $name) {
     }
 
-    function sayHello(string $nama = "Tod") : string {
+    function sayHello(string $nama) : string {
         return "Hallo $nama, nama saya {$this->name}" . PHP_EOL;
     }
 }
